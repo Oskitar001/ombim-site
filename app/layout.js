610b9888cc3703994c2b313fb460cabaa0f43c3b
Oskitar001 <o.martinez@ombim.com>
2026-03-10 13:58:49 +0100

    Fix Tailwind config to restore layout

--- app/layout.js
+++ app/layout.js
@@ -1,24 +1,14 @@
-// app/layout.jsx
+import './globals.css'
 
 export const metadata = {
-  title: "OMBIM · Tekla Structures & Automatizaciones BIM",
-  description:
-    "Especialistas en Tekla Structures, automatizaciones BIM y desarrollo de software a medida para ingenierías y constructoras.",
-};
+  title: 'OMBIM',
+  description: 'Tu descripción aquí',
+}
 
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
-      <head>
-        {/* Preconnect correcto para Google Fonts */}
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-
-        {/* Eliminado el preload incorrecto de Inter local */}
-        {/* Ya no intenta cargar /fonts/Inter-VariableFont.woff2 */}
-      </head>
-
       <body>{children}</body>
     </html>
-  );
+  )
 }

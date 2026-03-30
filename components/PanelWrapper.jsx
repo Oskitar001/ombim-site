14ec91a5a28ee2a840d251c6aa9f928484abf4bb
Oskitar001 <o.martinez@ombim.com>
2026-03-30 12:34:20 +0200

    por fin 80

--- components/PanelWrapper.jsx
+++ components/PanelWrapper.jsx
@@ -11,25 +11,27 @@ export default function PanelWrapper({ children }) {
 
   return (
     <>
-      {/* NAVBAR: siempre visible */}
+      {/* NAVBAR:
+          - En panel: ocultar en móvil (md:block), mostrar sólo escritorio.
+          - Fuera del panel: mostrar siempre. */}
       {isPanel ? (
-        <Navbar className="shadow-none border-b-0" />
+        <Navbar className="hidden md:block shadow-none border-b-0" />
       ) : (
         <Navbar />
       )}
 
-      {/* CONTENIDO Ajustado al tamaño REAL del navbar fijo */}
+      {/* Contenido principal.
+         Ajustado al navbar SOLO si está visible. */}
       <div
         className={
           isPanel
-            ? "pt-20 md:pt-[72px] bg-[#f3f4f6] dark:bg-[#242424] min-h-screen"
-            : "pt-20 md:pt-[72px] min-h-screen"
+            ? "pt-4 md:pt-[72px] bg-[#f3f4f6] dark:bg-[#242424] min-h-screen"
+            : "pt-[72px] min-h-screen"
         }
       >
         {children}
       </div>
 
-      {/* Footer solo en páginas públicas */}
       {!isPanel && <Footer />}
     </>
   );

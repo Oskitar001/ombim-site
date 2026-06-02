073b120ae443bfb161d7e690b5afed0dae0e08aa
Oskitar001 <o.martinez@ombim.com>
2026-06-02 10:52:17 +0200

    fix estilos inputs

--- app/panel/user/mis-datos/MisDatosClient.jsx
+++ app/panel/user/mis-datos/MisDatosClient.jsx
@@ -399,13 +399,18 @@ globalThis["btn-premium"] = `
 ====================================================== */
 
 /* input normal */
+
+
 const inputPremium = `
-  w-full px-3 py-2 rounded-lg
-  bg-gray-100 dark:bg-gray-800
-  border border-gray-300 dark:border-gray-700
-  focus:ring-2 focus:ring-blue-600
-  transition
+ w-full px-3 py-2 rounded-lg
+ bg-white text-gray-900
+ dark:bg-gray-800 dark:text-gray-200
+ border border-gray-300 dark:border-gray-700
+ focus:ring-2 focus:ring-blue-600
+ transition
 `;
+
+
 
 /* input disabled */
 const inputDisabled = `
